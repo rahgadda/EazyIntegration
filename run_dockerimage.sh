0fb391b29700107d9034a74fb7e7a2e663888ccb
rahgadda <gaddam.rahul@gmail.com>
2023-02-01 07:47:55 +0000

    Initial Draft

--- run_dockerimage.sh
+++ run_dockerimage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # -- Test docker file
-docker run -d -p 8080:8080 --name test-eazyintegration rahgadda/eazyintegration 
+docker run -d -p 8080:8080 -p 8888:8888  --name test-eazyintegration rahgadda/eazyintegration 
 curl --request POST --url http://httpbin.org/post --header 'content-type: application/json' --data '{"mytext": "message"}'
 curl --request POST --url http://localhost:8080/camel/api/post --header 'content-type: application/json' --data '{"test":"hello world"}'
 
 # -- Run in Debug using bash
-docker run --rm -it -p 8080:8080 rahgadda/eazyintegration bash
+docker run --rm -it -p 8080:8080 -p 8888:8888 rahgadda/eazyintegration bash
 docker exec -it rahgadda/eazyintegration rahgadda/eazyintegration bash
